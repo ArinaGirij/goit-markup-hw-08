06ef36a45cff6757ff603bc95110ed2baad8170a
ArinaGirij <94698485+ArinaGirij@users.noreply.github.com>
2022-04-26 16:07:53 +0300

    add

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,12 @@
-const refs = {
-  openModalBtn: document.querySelectorAll("[data-modal-open]"),
-  closeModalBtn: document.querySelector("[data-modal-close]"),
-  modal: document.querySelector("[data-modal]"),
-};
-
-refs.openModalBtn.forEach((el) => {
-  el.addEventListener("click", toggleModal);
-});
-
-refs.closeModalBtn.addEventListener("click", toggleModal);
-
-function toggleModal() {
-  refs.modal.classList.toggle("is-hidden");
-}
+(() => {
+	const refs = {
+		openMenuBtn: document.querySelector('[data-menu-open]'),
+		closeMenuBtn: document.querySelector('[data-menu-close]'),
+		menu: document.querySelector('[data-menu]'),
+	};
+	refs.openMenuBtn.addEventListener('click', toggleModal);
+	refs.closeMenuBtn.addEventListener('click', toggleModal);
+	function toggleModal() {
+		refs.menu.classList.toggle('is-open');
+	}
+})();
